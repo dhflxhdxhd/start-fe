51a73e76647895c3f20bcbff2eb81c7ea38609c2
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-03-24 17:40:29 +0900

    3주차 퀴즈
     마지막 문제 제외

--- 03/quiz.js
+++ 03/quiz.js
@@ -11,4 +11,52 @@ console.log(av)
 
 // 구구단을 출력하는 코드를 for구문을 사용하여 작성하세요 
 
-// for (let i = 1; i < )
+// for (let i = 1; i < 10; i++){
+//     for (let j = 1; j < 10; j++ ){
+//         console.log(`${i} X ${j} = ${i*j} `)
+//     }
+// }
+
+// // 
+// function gugu(){
+//     for (let i = 1; i < 10; i++){
+//         for (let j = 1; j < 10; j++ ){
+//             console.log(`${i} X ${j} = ${i*j} `)
+//         }
+//     }
+// }
+
+// gugu()
+
+// 
+var CRUD = {
+    create : function create(...todo){
+        var n = arguments.length
+        if (n>1) {
+            for ( var cnt = 0;  cnt < n; cnt++){
+                todos.push(arguments[cnt]);
+            }
+        }else{
+            todos.push(arguments[0]);
+        }
+    },
+    read : function read(){
+        todos.forEach(function(todo){
+            console.log(todo);
+        });
+    },
+    update : function update(updateTodo, newTodo){
+        var updateIndex = todos.findIndex((todo) => {
+            return todo === updateTodo;
+        });
+    
+        todos[updateIndex] = newTodo;
+    },
+    delete : function del(deleteTodo){
+        todos = todos.filter((todo) => {
+            return todo !==  deleteTodo;
+        }
+    );},
+}
+
+// 
