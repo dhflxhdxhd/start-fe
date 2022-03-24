ec392146e5a2e591985ce10db82d1f934850a6ce
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-03-24 16:18:06 +0900

    3주차 퀴즈 1번

---
+++ 03/quiz.js
@@ -0,0 +1,14 @@
+// 3주차 퀴즈
+
+//  var count = [10,20,30,40,50,60,70,100]; 배열 count 요소들의 평균값 구하는 코드를 작성하세요 *
+const count = [10,20,30,40,50,60,70,100];
+let sum = 0
+for (let i = 0; i < count.length; i++){
+    sum += count[i]
+}
+const av = sum / count.length
+console.log(av)
+
+// 구구단을 출력하는 코드를 for구문을 사용하여 작성하세요 
+
+// for (let i = 1; i < )
