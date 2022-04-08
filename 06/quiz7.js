e8e033b61d26a47c4b5749498a2d853f4956c4bb
김아현 <dhflxhdxhd@gmail.com>
2022-04-08 17:23:27 +0900

    box 랜덤 위치 변경

--- 06/quiz7.js
+++ 06/quiz7.js
@@ -1,13 +1,26 @@
-const box =document.querySelector(".box")
+const box = document.querySelector(".box");
+const bug = document.querySelector("#bug");
 const point = document.querySelector("#point");
 const life = document.querySelector("#life")
+
+
+function getRandom(max){
+    return Math.floor(Math.random()*max);
+}
+
+function showBox(){
+    const x = getRandom(box.offsetWidth - bug.offsetWidth);
+    const y = getRandom(box.offsetHeight - bug.offsetHeight);
+
+    bug.style.left = `${x}px`;
+    bug.style.top = `${y}px`;
+}
 
 function clickBoxEvent(event){
     const target = event.target;
     let score_point = parseInt(point.innerText);
     let score_life = parseInt(life.innerText);
     
-
     if (target === event.currentTarget.querySelector("#bug")){
         score_point += 1
         point.innerHTML = score_point;
@@ -19,8 +32,16 @@ function clickBoxEvent(event){
         if (score_life === 0){
             alert("gameover");
         }
-  
+
     }
 }
 
-box.addEventListener('click', clickBoxEvent);
+function init(){
+    setInterval(showBox, 2000);
+    box.addEventListener('click', clickBoxEvent);
+}
+
+init();
+
+
+
