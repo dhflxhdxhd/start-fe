14b0695cdc6ec21af9cf4466989693ab8622c275
or.llon <52151533+dhflxhdxhd@users.noreply.github.com>
2022-04-14 17:29:55 +0900

    7주차

---
+++ 07/app.js
@@ -0,0 +1,20 @@
+/* this
+java : 자신을 가리키는 참조 변수
+javascript : 해당 함수 호출 방식에 따라 this에 바인딩 되는 객체가 달라짐
+*/
+// const test = {
+//     prop: 42,
+//     function() {
+//         return this.prop;
+//     },
+// }
+
+// console.log(test.function());
+
+// 함수호출
+// const foo = function(){
+//     console.log(this) //전역 객체에 바인딩된다. 
+// }
+
+// 클래스는 언제 사용하는 것이 좋은가
+// 함수와 클래스 차이
